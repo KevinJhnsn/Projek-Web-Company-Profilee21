b09eb12e68f30246476ee7545890a1071a74373c
Will <willy.rafaelfs@gmail.com>
2025-07-20 12:36:42 +0700

    Create ServiceModel

---
+++ app/Models/ServiceModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ServiceModel extends Model
+{
+    protected $table      = 'services';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['title', 'description'];
+    protected $useTimestamps = true;
+}
