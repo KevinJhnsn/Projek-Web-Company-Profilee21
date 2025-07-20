57e3c2fd26c4e666082383e966548d5dab0fb086
Will <willy.rafaelfs@gmail.com>
2025-07-20 12:51:03 +0700

    Create db_products

--- app/Database/Migrations/2025-07-20-054156_CreateProductsTable.php
+++ app/Database/Migrations/2025-07-20-054156_CreateProductsTable.php
@@ -8,11 +8,20 @@ class CreateProductsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'          => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'name'        => ['type' => 'VARCHAR', 'constraint' => 150],
+            'description' => ['type' => 'TEXT'],
+            'price'       => ['type' => 'DECIMAL', 'constraint' => '10,2', 'null' => true],
+            'created_at'  => ['type' => 'DATETIME', 'null' => true],
+            'updated_at'  => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('products');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('products');
     }
 }
