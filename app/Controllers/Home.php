a9edd2895d50a002c40037545e6a635a7b3d8124
Will <willy.rafaelfs@gmail.com>
2025-07-20 12:35:39 +0700

    update controllers home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('frontend/home');
+    }
+
+    public function about(): string
+    {
+        return view('frontend/about');
     }
 }
