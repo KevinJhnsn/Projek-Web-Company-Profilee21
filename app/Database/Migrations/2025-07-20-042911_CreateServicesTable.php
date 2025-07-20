62bf531577c94202adc86881880dc87b9030b641
Will <willy.rafaelfs@gmail.com>
2025-07-20 12:33:21 +0700

    Create db_services

---
+++ app/Database/Migrations/2025-07-20-042911_CreateServicesTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateServicesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'title'       => ['type' => 'VARCHAR', 'constraint' => 150],
+            'description' => ['type' => 'TEXT'],
+            'created_at'  => ['type' => 'DATETIME', 'null' => true],
+            'updated_at'  => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('services');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('services');
+    }
+}
